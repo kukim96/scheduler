8d1004ae3c10842a342769e210efc3f8265dd6ed
Kiup Kim <kukim96@gmail.com>
2021-05-10 17:06:34 -0600

    built, styled, and tested DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import classnames from "classnames";
+
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  const DayListItemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return `no spots remaining`
+    } else if (spots === 1) {
+      return `1 spot remaining`
+    } else {
+      return `${spots} spots remaining`
+    }
+  }
+
+  return (
+    <li 
+      className={DayListItemClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2 className="text-regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
