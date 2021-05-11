e1ddfdcac7ab5696a8d9e6dd36561ce5924a86ae
Kiup Kim <kukim96@gmail.com>
2021-05-11 17:18:49 -0600

    iterate over appointment andspread props

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
 
 import "components/Application.scss";
-import "components/DayList"
+
 import DayList from "components/DayList";
+import Appointment from "components/Appointment"
 
 const days = [
   {
@@ -20,6 +21,53 @@ const days = [
     name: "Wednesday",
     spots: 0,
   },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Daniel Kim",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Serah Lee",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
 ];
 
 export default function Application(props) {
@@ -49,6 +97,13 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+        ))}
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
