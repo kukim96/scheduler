b7bc2e5bda9005543c1536e07807f41bbc48a486
Kiup Kim <kukim96@gmail.com>
2021-05-14 20:26:16 -0600

    updating spots remaining

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -23,6 +23,11 @@ export default function useApplicationData() {
     })
   }, []);
 
+  useEffect(() => {
+    axios.get("/api/days")
+      .then((days) => setState((state) => ({...state, days: days.data})));
+  }, [state.appointments])
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
