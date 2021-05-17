229a3d20a0913e93ff8be039f78460cd0a1d5cab
Kiup Kim <kukim96@gmail.com>
2021-05-17 13:05:51 -0600

    Type Checking with Prop Types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import PropTypes from 'prop-types';
+
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss";
@@ -23,3 +25,7 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
